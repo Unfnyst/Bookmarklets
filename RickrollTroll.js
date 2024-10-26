14f596158f6e5772cb9392e3aaaa7f91d92dc426
Unfnyst <minaadel23102013@hotmail.com>
2024-10-26 10:50:56 +0300

    Create RickrollTroll.js

--- RickrollTroll.js
+++ RickrollTroll.js
@@ -1,1 +1,1 @@
-javascript:(function()prompt("What link do you want to go to?");location.href="https://youtu.be/T7B2KOdIYdk";})();
+javascript:(function(){prompt("Which link do you want to go to?");location.href="https://youtu.be/T7B2KOdIYdk";})();
